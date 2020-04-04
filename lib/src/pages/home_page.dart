8e914b83305f67b4fc6efd86495f9fad383b768b
H Leonardo A Corredor <hleonard31@gmail.com>
2020-04-04 10:58:36 -0500

    Creación y ajustes de bottomNavigationBar
    Se adiciona el el widget BottomNavigationBar al home page
    
    Se crea las paginas:
    - Mapas
    - Direcciones
    
    Se ajusta la navegación del BottomNavigationBar dependiendo la opción seleccionada a través de la creación de la variable currentIndex en el HomePage.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:scanner_qr_skinnet/src/pages/direcciones_page.dart';
+import 'package:scanner_qr_skinnet/src/pages/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola mundo'),
-      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
     );
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones')
+        )
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+        break;
+      case 1:
+        return DireccionesPage();
+        break;
+      default:
+        return MapasPage();
+        break;
+    }
   }
 }
