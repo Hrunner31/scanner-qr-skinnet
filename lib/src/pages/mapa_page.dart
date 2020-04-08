8504e94453485e0c3ea22824bf99d19a106e121c
H Leonardo A Corredor <hleonard31@gmail.com>
2020-04-07 20:15:06 -0500

    Se implementa metodo para cambiar de tipo de mapa en la aplicación

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:scanner_qr_skinnet/src/models/scan_model.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
+
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  
+  final mapaCtrl = new MapController();
+
+  String tipoMapa = 'streets';
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +25,20 @@ class MapaPage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.my_location), 
-            onPressed: () {}
+            onPressed: () {
+              mapaCtrl.move(scan.getLatLng(), 15);
+            }
           )
         ],
       ),
       body: _crearFlutterMap(scan),
+      floatingActionButton: _crearBotonFlotante(context),
     );
   }
 
   Widget _crearFlutterMap(ScanModel scan) {
     return FlutterMap(
+      mapController: mapaCtrl,
       options: MapOptions(
         center: scan.getLatLng(),
         zoom: 10
@@ -42,7 +56,7 @@ class MapaPage extends StatelessWidget {
       '{id}/{z}/{x}/{y}@2x.png?access_token={accesToken}',
       additionalOptions: {
         'accesToken': 'pk.eyJ1IjoiaHJ1bm5lcjc2IiwiYSI6ImNrOHFsN2VpdDAxcWwzZnF1bXIwYmVjNngifQ.VGmUBEr8HSWD10adq77vIA',
-        'id': 'mapbox.streets' //street, dark, light, outdoors, satellite
+        'id': 'mapbox.$tipoMapa' //street, dark, light, outdoors, satellite
       }
 
     );
@@ -64,6 +78,40 @@ class MapaPage extends StatelessWidget {
           )
         )
       ]
+    );
+  }
+
+  Widget _crearBotonFlotante(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.repeat),
+      backgroundColor: Theme.of(context).primaryColor,
+      onPressed: () {
+        switch (tipoMapa) {
+          case 'streets':
+            tipoMapa = 'dark';
+          break;
+          case 'dark':
+            tipoMapa = 'light';
+          break;
+          case 'light':
+            tipoMapa = 'outdoors';
+          break;
+          case 'outdoors':
+            tipoMapa = 'satellite';
+          break;
+          case 'satellite':
+            tipoMapa = 'streets';
+          break;
+          default:
+            tipoMapa = 'streets';
+          break;
+          //street, dark, light, outdoors, satellite
+        }
+
+        setState(() {
+          
+        });
+      }
     );
   }
 }
